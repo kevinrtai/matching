cd569a042cfb9dda5a7b35f3f9aabe3695c4f766
Kevin Tai <me@kevinrtai.dev>
2019-08-11 16:37:21 -0400

    Modified output format

--- smp/main.py
+++ smp/main.py
@@ -21,7 +21,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Solve the Stable Marriage Problem')
     parser.add_argument('women_prefs', metavar='W', type=str)
     parser.add_argument('men_prefs', metavar='M', type=str)
-    parser.add_argument('--n', default=1000, type=int)
+    parser.add_argument('-n', '--num_exp', dest='n', default=1000, type=int)
     parser.add_argument('--weight', default=0.5, type=float, help='weight to be assigned to the women\'s score')
     parser.add_argument('--scorer', default='one_zero', choices=scorers)
     parser.add_argument('--warper', default='identity', choices=warpers)
@@ -53,7 +53,7 @@ def main():
     m = read_input(args.men_prefs)
     problem_size = len(m)
    
-    print(f'Starting experiments, n={args.n}')
+    print(f'Solving...')
 
     results = []
     best_score = best = None
@@ -90,7 +90,9 @@ def main():
     print('# RESULTS #')
     print('###########')
     print(f'Top Score: {best_score:.2%}')
-    print(f'Best Matches:\n{best}')
+    print(f'Best Matches:')
+    for w in sorted(best.keys()):
+        print(f'    {w} - {best[w]}')
 
     # Save parameters, outputs in dict
     output = {
@@ -109,6 +111,7 @@ def main():
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
     pickle.dump(output, open(f'outputs/results{timestamp}.smp', 'wb'))
 
+    print('\nfin')
 
 if __name__ == '__main__':
     main()
